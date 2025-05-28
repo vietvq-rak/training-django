0c78571526baa1eae85dde984e87bd394b840595
vietvq-rak <vietvq@rakumo.vn>
2025-05-28 14:45:06 +0700

    Create a Django Views(Createview, Listview, Detailview, Updateview, Deleteview, Formview)

---
+++ Practice_Django/shop/store/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    order_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer.name}"
