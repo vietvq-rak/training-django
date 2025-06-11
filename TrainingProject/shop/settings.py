91a985e1e0ff01e6c8044ed4e929a05ceda6e881
vietvq-rak <vietvq@rakumo.vn>
2025-06-11 16:41:46 +0700

    Create, Delete, Update, List category features

--- TrainingProject/shop/settings.py
+++ TrainingProject/shop/settings.py
@@ -139,6 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vietvo.100827@gmail.com'
-EMAIL_HOST_PASSWORD = 'rong jzbl feil fhff'
+# EMAIL_HOST_USER = 'vietvo.100827@gmail.com'
+# EMAIL_HOST_PASSWORD = 'rong jzbl feil fhff'
 
